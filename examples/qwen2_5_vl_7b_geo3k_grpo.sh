a94518e59ab7feb29dfa4f7e3b92868136295e92
Cheryl Liang <40263198+cheryyunl@users.noreply.github.com>
2025-06-11 18:24:06 -0700

    Update qwen2_5_vl_7b_geo3k_grpo.sh

--- examples/qwen2_5_vl_7b_geo3k_grpo.sh
+++ examples/qwen2_5_vl_7b_geo3k_grpo.sh
@@ -7,7 +7,7 @@ export PYTHONUNBUFFERED=1
 MODEL_PATH=Qwen/Qwen2.5-VL-7B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
-    config=examples/config.yaml \
+    config=examples/config_math.yaml \
     data.train_files=hiyouga/geometry3k@train \
     data.val_files=hiyouga/geometry3k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
