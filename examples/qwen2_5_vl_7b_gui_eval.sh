9edc000d1bb964ef5d29d552c74276de998dae18
Cheryl Liang <40263198+cheryyunl@users.noreply.github.com>
2025-06-27 11:07:24 -0700

    Update qwen2_5_vl_7b_gui_eval.sh

--- examples/qwen2_5_vl_7b_gui_eval.sh
+++ examples/qwen2_5_vl_7b_gui_eval.sh
@@ -10,6 +10,7 @@ python3 -m verl.trainer.main \
     config=examples/config_eval.yaml \
     data.train_files=/code/android_control/data@validation \
     data.val_files=/code/android_control/data@test \
+    worker.rollout.max_num_batched_tokens=22528 \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=qwen2_5_vl_7b_gui_grpo_eval \
     trainer.n_gpus_per_node=8
