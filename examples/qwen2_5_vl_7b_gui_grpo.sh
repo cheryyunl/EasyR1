ef04fc84626fbe02658024e117be047ff32647ce
Yongyuan Liang <yongyuanl@Yongyuans-MacBook-Pro.local>
2025-07-10 16:18:30 -0700

    update

--- examples/qwen2_5_vl_7b_gui_grpo.sh
+++ examples/qwen2_5_vl_7b_gui_grpo.sh
@@ -6,7 +6,7 @@ export PYTHONUNBUFFERED=1
 MODEL_PATH=Qwen/Qwen2.5-VL-7B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
-    config=examples/config.yaml \
+    config=examples/config_gui.yaml \
     data.train_files=/code/android_control/data@train \
     data.val_files=/code/android_control/data@test \
     worker.actor.model.model_path=${MODEL_PATH} \
